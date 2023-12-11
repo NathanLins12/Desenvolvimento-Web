7461d5d0b3cf4f32bce46a3d8087431e2bb4dfd9
Nathan Lins <nathan.lins012@gmail.com>
2023-12-11 20:18:13 -0300

    add handle submit and aux name

--- 5-React/class-03-states/src/App.tsx
+++ 5-React/class-03-states/src/App.tsx
@@ -4,12 +4,16 @@ type UserData = {
   name: string;
   avatar_url: string;
   bio: string;
-} 
+};
 
 export function App() {
   const [count, setCount] = useState(0);
-  const [name, setName] = useState("");
+  const [name, setName] = useState("NathanLins12");
+  const [auxName, setAuxName] = useState("");
   const [data, setData] = useState<UserData>({} as UserData);
+
+  let count = 0;
+  console.log(count);
 
   function addCount() {
     // Closures
@@ -21,12 +25,20 @@ export function App() {
     console.log(event);
   }
 
+  function handleSubmit() {
+    event?.preventDefault()
+    setAuxName(name);
+  }
+
   useEffect(() => {
     // Hook
-    fetch(`https://api.github.com/users/${name}`)
+    fetch(`https://api.github.com/users/${auxName}`)
       .then((response) => response.json())
       .then((data) => setData(data));
+    console.log("effect");
   }, [name]);
+
+  console.log(data);
 
   return (
     <>
@@ -34,7 +46,15 @@ export function App() {
       <p>name: {name}</p>
       <p>Count: {count}</p>
       <button onClick={addCount}>Add&gt;</button>
-      <input type="text" onChange={handleChangeName} />
+
+      <form
+        onSubmit={handleSubmit}
+        style={{ textAlign: "center", marginTop: 24 }}
+      >
+        <input type="text" onChange={handleChangeName} />
+        <button>Pesquisar</button>
+      </form>
+
       {data && (
         <section>
           <h2>{data.name}</h2>
