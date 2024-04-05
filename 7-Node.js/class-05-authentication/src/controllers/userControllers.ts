e76a8b6114dafc5d14babb722140601cbe3af527
NathanLins12 <nathan.lins012@gmail.com>
2024-04-05 20:35:12 -0300

    add server errors and user create validation

--- 7-Node.js/class-05-authentication/src/controllers/userControllers.ts
+++ 7-Node.js/class-05-authentication/src/controllers/userControllers.ts
@@ -1,8 +1,45 @@
-import { Request, Response, NextFunction } from "express"
+import { Request, Response, NextFunction } from "express";
+import { z } from "zod";
 
 export const userControllers = {
   async create(req: Request, res: Response, next: NextFunction) {
     try {
+      const userSchema = z
+        .object({
+          name: z
+            .string({
+              required_error: "Nome obrigatório!",
+              invalid_type_error: "Para o nome use somente texto!",
+            })
+            .min(3, { message: "Mínimo de 3 caracteres para o nome!" })
+            .max(255, "Tamanho máximo atingido por nome"),
+
+          email: z
+            .string({
+              required_error: "Email obrigatório!",
+              invalid_type_error: "Somente texto!",
+            })
+            .email({ message: "Email inválido!" })
+            .max(255, "Tamanho máximo atingido por email"),
+
+          password: z
+            .string({
+              required_error: "Senha obrigatória!",
+              invalid_type_error: "Para a senha use o tipo string!",
+            })
+            .min(7, "Senha com mínimo de 7 caracteres")
+            .regex(/^(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d]).{7,}$/, {
+              message:
+                "A senha deve conter pelo menos uma letra maiúscula, um número e um caractere especial.",
+            })
+            .max(255, "Tamanho máximo atingido por por senha"),
+        })
+        .strict();
+
+      const { name, email, password } = userSchema.parse(req.body);
+      console.log(name, email, password);
+      
+
       return res.status(201).json({ message: "User created!" });
     } catch (error) {
       return next(error);
--- 7-Node.js/class-05-authentication/src/server.ts
+++ 7-Node.js/class-05-authentication/src/server.ts
@@ -1,11 +1,17 @@
 import express from "express";
 import "dotenv/config";
 import { routes } from "./routes";
+import { pageNotFound } from "./errors/pageNotFound";
+import { appErrors } from "./errors/appErrors";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(routes)
+app.use(express.json());
+app.use(routes);
+
+app.use(pageNotFound);
+app.use(appErrors);
 
 app.listen(PORT, () => {
   console.log(`Server conected...`);
